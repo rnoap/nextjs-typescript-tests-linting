5ff9135a2ccdaa3732bfb7defed8e9920bfb1428
Raúl Noa Pedroso <rnoap13@gmail.com>
2023-03-28 09:49:02 -0300

    Added | jest coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,28 @@ const customJestConfig = {
     // Add more setup options before each test is run
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     testEnvironment: 'jest-environment-jsdom',
+    coverageDirectory: "test-coverage", //new folder name instead of the default name "coverage"
+    reporters: [
+        [
+            "jest-html-reporters",
+            {
+                "publicPath": "./html-report", //folder where this reporter is going to be generated
+                "filename": "report.html",
+                "openReport": false, //Change to true if we want to open the report automatically
+                "urlForTestFiles": "../",
+            }
+        ]
+    ],
+    coverageThreshold: {
+        "global": {
+            "branches": 80,
+            "functions": 80,
+            "lines": 80,
+            "statements": 80
+        }
+    },
 }
+
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig)
